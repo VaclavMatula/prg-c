af62196bd2d7120771c81903a01a572f69cda654
VaclavMatula <76968241+VaclavMatula@users.noreply.github.com>
2021-01-05 17:18:22 +0100

    Create Vypise_libovolne_cislo,.c

---
+++ Vypise_libovolne_cislo,.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("zadejte celé číslo: ");  
+    scanf("%d", &number);
+    printf("zadali jste: %d", number);
+    
+    return 0;
+}
